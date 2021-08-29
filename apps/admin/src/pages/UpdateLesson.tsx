785d0810b52467d974be96719f0d180a4fcaf9a5
jamilur-r <md.jamilur.rahman@g.bracu.ac.bd>
2021-08-30 01:56:56 +0600

    fix

--- apps/admin/src/pages/UpdateLesson.tsx
+++ apps/admin/src/pages/UpdateLesson.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { api_key, api_url, Colors } from '@skill-mask/app';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
