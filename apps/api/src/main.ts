c6b1abc46e08f87487ed3c10b95ff773a65aaf96
jamilur-r <md.jamilur.rahman@g.bracu.ac.bd>
2021-09-01 23:02:42 +0600

    add

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,20 @@
 import * as express from 'express';
 import { Application } from 'express';
 import * as cors from 'cors';
-import { connect } from 'mongoose';
+import { MongoClient } from 'mongodb';
 import * as morgan from 'morgan';
 import * as path from 'path';
 import { json, urlencoded } from 'body-parser';
 import { environment } from './environments/environment';
 import { RootRouter } from './routes';
 import compression = require('compression');
+// const db_url: string = process.env.db_url || environment.db_url;
+const db_url: string =
+process.env.db_url ||
+'mongodb+srv://jamilur:keepaway1147@test-skill-sm.kqcbr.mongodb.net/skillMask?retryWrites=true&w=majority';
 
-const db_url: string = process.env.db_url || environment.db_url;
+const client = new MongoClient(db_url)
+
 const port: string = process.env.port || environment.port;
 
 const app: Application = express();
@@ -25,10 +30,22 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api', RootRouter);
 
-connect(db_url, {})
-  .then(() => console.log('DB CONNECTED'))
-  .catch((err) => console.log(err));
-
+// connect(db_url, {})
+//   .then(() => console.log('DB CONNECTED'))
+//   .catch((err) => console.log(err));
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("skillMask").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port);
 });
