ad2f378beb09019a7259fcec116b8ee18b350377
jamilur-r <md.jamilur.rahman@g.bracu.ac.bd>
2021-09-09 02:27:50 +0600

    bhvh

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -21,6 +21,21 @@ const db_url: string = process.env.db_url || environment.db_url;
 const port: string = process.env.port || environment.port;
 
 const app: Application = express();
+app.use(function(req, res, next) {  
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
+  );
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
+});
+
 app.use(compression());
 app.use(morgan('tiny'));
 app.use(cors());
