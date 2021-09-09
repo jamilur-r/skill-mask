78eb2cd5a152082f4d3e010f69f877f6c1c0eb66
jamilur-r <md.jamilur.rahman@g.bracu.ac.bd>
2021-09-09 23:18:59 +0600

    sh styles

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/sh     
+echo -e "Going to Project Dir \n "
 cd /home/skill-mask
+echo -e "Pulling latest Files \n"
 sudo git pull
+echo -e "Installing dependencies \n"
 sudo yarn install
+echo -e "Building Project \n"
 sudo yarn build
+echo -e "Restarting nginx \n"
 sudo systemctl restart nginx
+echo -e "Restarting PM2 \n"
 pm2 restart skill-mask
